5f37f134db3acb6c233617ccf62334fe498f39db
luyishuang <1781990652@qq.com>
2019-09-18 19:27:40 +0800

    lu

--- （2）NodeJS全局API/loopTimer.js
+++ （2）NodeJS全局API/loopTimer.js
@@ -1,7 +1,12 @@
-setInterval(function loop(){
-    console.log('i will loop forever!');
+function loop(){
+    console.log("I will loop forever!");
+}
+
+setInterval(function(){
+    loop();
 },500);
+
 setTimeout(function(){
-    console.log("game over");
-    process.exit()
-},5000)
+    console.log("Game over")
+    process.exit();
+},5000);
